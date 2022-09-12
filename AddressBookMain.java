78eb72a20725ffa874069beb11c2670ddd24ae8c
unknown <sidhodhank2018@gmail.com>
2022-09-12 13:09:17 +0530

    Create Contact in Address Book

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -1,20 +1,29 @@
 package addressBookMain;
-
-
-
-
-
 public class AddressBookMain {
-	public static void main(String args[])   
-	{  
-		System.out.println(""
-    			+ "    ##     ## ###### ##     ###### ###### ###   ### ######\r\n"
-    			+ " *  ##     ## ##     ##     ##     ##  ## ## # # ## ##      *\r\n"
-    			+ "*** ##  #  ## #####  ##     ##     ##  ## ##  #  ## #####  ***\r\n"
-    			+ " *  ## # # ## ##     ##     ##     ##  ## ##     ## ##      *\r\n"
-    			+ "    ###   ### ###### ###### ###### ###### ##     ## ###### \r\n"
-    			+ "-------------------WELCOME TO ADDRESS BOOK-----------------------\r\n");
-	}  
+
+	  public static void main(String[] args) {
+			   String FirstName="Sidhodhan";
+			   String LastName="Kamble";
+			   String Address="At Post Bholawade";
+			   String City="Pune";
+			   String State="Maharashtra";
+			   String Zip="412206";
+			   String PhoneNumber="9354788125";
+			   String Email="hsjsj@gmail.com";
+	  
+	   System.out.println(""
+		    			+ "    ##     ## ###### ##     ###### ###### ###   ### ######\r\n"
+		    			+ " *  ##     ## ##     ##     ##     ##  ## ## # # ## ##      *\r\n"
+		    			+ "*** ##  #  ## #####  ##     ##     ##  ## ##  #  ## #####  ***\r\n"
+		    			+ " *  ## # # ## ##     ##     ##     ##  ## ##     ## ##      *\r\n"
+		    			+ "    ###   ### ###### ###### ###### ###### ##     ## ###### \r\n"
+		    			+ "-------------------WELCOME TO ADDRESS BOOK-----------------------\r\n");
+		System.out.println("Contact1 [FirstName=" + FirstName + ", LastName=" + LastName + ", Address=" + Address + ", City="
+				+ City + ", State=" + State + ", Zip=" + Zip + ", PhoneNumber=" + PhoneNumber + ", Email=" + Email
+				+ "]");
+	}
+	}
+	
 
 
-}
+
