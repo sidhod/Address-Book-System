a9af91a152a009c2cceb7e09cf7402b7e3c690ae
unknown <sidhodhank2018@gmail.com>
2022-09-12 11:40:52 +0530

    Welcome to Address Books

---
+++ AddressBookMain.java
@@ -0,0 +1,20 @@
+package addressBookMain;
+
+
+
+
+
+public class AddressBookMain {
+	public static void main(String args[])   
+	{  
+		System.out.println(""
+    			+ "    ##     ## ###### ##     ###### ###### ###   ### ######\r\n"
+    			+ " *  ##     ## ##     ##     ##     ##  ## ## # # ## ##      *\r\n"
+    			+ "*** ##  #  ## #####  ##     ##     ##  ## ##  #  ## #####  ***\r\n"
+    			+ " *  ## # # ## ##     ##     ##     ##  ## ##     ## ##      *\r\n"
+    			+ "    ###   ### ###### ###### ###### ###### ##     ## ###### \r\n"
+    			+ "-------------------WELCOME TO ADDRESS BOOK-----------------------\r\n");
+	}  
+
+
+}
